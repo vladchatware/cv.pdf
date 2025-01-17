4efc80c34db5dd546ac65d96d1f1727ceb939589
ading2210 <ading2019@gmail.com>
2025-01-17 16:04:21 -0500

    load emulator files from memory

--- build.sh
+++ build.sh
@@ -11,7 +11,19 @@ fi
 
 source ./emsdk/emsdk_env.sh >/dev/null 2>&1
 
-if [ "$1" = "clean" ]; then
-  emmake make -C tinyemu/ -f Makefile.js clean
+mkdir -p out build
+if [ ! -f "build/vm.tar.gz" ]; then
+  wget "https://bellard.org/tinyemu/diskimage-linux-riscv-2018-09-23.tar.gz" -O build/vm.tar.gz
 fi
-emmake make -C tinyemu/ -f Makefile.js -j$(nproc --all)
+if [ ! -d "build/vm" ]; then
+  tar -xf build/vm.tar.gz -C build
+  mv build/diskimage* build/vm
+fi
+
+if [ "$1" = "clean" ]; then
+  emmake make -C tinyemu/ -f Makefile.pdfjs clean
+fi
+emmake make -C tinyemu/ -f Makefile.pdfjs -j$(nproc --all)
+
+python3 embed_files.py file_template.js vm.cfg build/vm/bbl64.bin build/vm/kernel-riscv64.bin build/vm/root-riscv64.bin build/files.js
+cat tinyemu/js/riscvemu64.js build/files.js pdflinux.js > out/compiled.js
