fbc31f63ebe11f6b4978b32ea85cb502b9d65e61
Vlad Rimsha <vlad@jelly.ninja>
2025-04-12 15:46:27 +0700

    add references

---
+++ pdf.ts
@@ -0,0 +1,19 @@
+import puppeteer from 'puppeteer'
+
+const headless = process.argv[2] === '--headless' || false
+const browser = await puppeteer.launch({headless})
+const pages = await browser.pages()
+const page = pages[0]
+
+if (headless) {
+  await page.goto(`file://${__dirname}/cv.md`)
+  await page.pdf({
+    path: 'cv.pdf',
+  })
+  await Bun.$`open ./cv.pdf`.quiet()
+} else {
+  await page.goto('https://github.com/jellyninjadev/cv.pdf/releases/download/v1.0.0/cv.pdf')
+  await new Promise(() => {})
+}
+
+await browser.close()
